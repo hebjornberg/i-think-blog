05d218faff08f379acbf5698f6247d8ef0450232
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-03 20:56:56 +0000

    Add excerpt and updated

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-hebjornberg-ithinkblog-vwm0jkf7snq.ws.codeinstitute-ide.net',
 '.herokuapp.com']
