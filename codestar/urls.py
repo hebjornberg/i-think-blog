6f76eba2f7fb4d15c85017f474bbb0e487761e0f
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-04 17:20:05 +0000

    Add html-structure

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as blog_views
+from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', blog_views.my_blog, name='blog'),
+    path("", include("blog.urls"), name='blog-urls'),
     path('admin/', admin.site.urls),
 ]
