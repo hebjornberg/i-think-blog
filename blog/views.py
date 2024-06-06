d1e00395a78d4d44297a359aefae298c9977bdd0
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-06 16:45:03 +0000

    Change code in index.html

--- blog/views.py
+++ blog/views.py
@@ -9,25 +9,18 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 def post_detail(request, slug):
-
-    """ 
+    """
     Display an individual :model:`blog.Post`.
 
     **Context**
 
-    ``post```
-    An instance of :model:`blog.Post`.
+    ``post``
+        An instance of :model:`blog.Post`.
 
-    **Template**
+    **Template:**
 
-    :template:`blo/post_detail.html
-
+    :template:`blog/post_detail.html`
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request, 
-        "blog/post_detail.html", 
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
