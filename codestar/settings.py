c89fc6f2c2e5074348bb4afe98f1e7a66fad6472
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-11 16:43:12 +0000

    added authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-hebjornberg-ithinkblog-vwm0jkf7snq.ws.codeinstitute-ide.net',
 '.herokuapp.com']
