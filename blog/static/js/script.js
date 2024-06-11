4bb0adda954596b48145d409d4280dbf1c463e8a
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-11 20:53:36 +0000

    Add comment CRUD

--- blog/static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Amor y vida")
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic 
+from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
-from django.contrib.auth.models import User
 
 # Create your views here.
-class PostList(generic.ListView): 
+
+
+class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -30,7 +32,6 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -38,28 +39,23 @@ def post_detail(request, slug):
             comment.post = post
             comment.save()
             messages.add_message(
-                request, messages.SUCCESS, 
+                request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
     
     comment_form = CommentForm()
 
-    def profile_page(request):
-        user = get_object_or_404(User, user=request.user)
-        comments = user.commenter.all()
-
-    print("About to return render template")    
-
     return render(
         request,
         "blog/post_detail.html",
         {
-        "post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
         },
     )
+
 
 def comment_edit(request, slug, comment_id):
     """
@@ -71,19 +67,21 @@ def comment_edit(request, slug, comment_id):
         post = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
-    
-    if comment_form.is_valid() and comment.author == request.user:
-        comment = comment_form.save(commit=False)
-        comment.post = post
-        comment.approved = False
-        comment.save()
-        messages.add_message(request, messages.SUCCESS, 'Comment Uploaded!')
-    else: 
-        messages.add_message(request, messages.SUCCESS, 'Error updating comment!')
-    return HttpResponseRedirect(reverse('post_detail', arg=[slug]))
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
-def comment_delete(request, slug, comment_id): 
+def comment_delete(request, slug, comment_id):
     """
     view to delete comment
     """
@@ -94,9 +92,9 @@ def comment_delete(request, slug, comment_id):
     if comment.author == request.user:
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
-    else: 
-        messages.add_message(request, messages.ERROR, 'You can only delete your owns comments!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
-    return HttpResponseRedirect(reverse('post_detail', arg=[slug]))
-    
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
